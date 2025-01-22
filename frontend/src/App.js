df18a0d3afc5129e9b5bd3fdbede7288862586ad
seedm24 <despotic62@gmail.com>
2025-01-22 22:12:36 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { getHelloMessage } from "./api";
+
+function App() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        getHelloMessage().then((data) => setMessage(data.message));
+    }, []);
+
+    return (
+        <div>
+            <h1>React + FastAPI</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default App;
 
